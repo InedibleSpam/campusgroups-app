74227f3aac02e451e58c98aafd59c94d546b6ecd
isaac-gasparri-27 <isaac.gasparri26@houghton.edu>
2025-05-01 15:03:38 -0400

    Update App.jsx

--- login-screen/src/App.jsx
+++ login-screen/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
-import ReactDOM from 'react-dom'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import Login from './components/Login'
 import { Route, Routes } from 'react-router-dom'
 import Register from './components/Register'
 import Homepage from './components/Homepage'
+import Navbar from "./components/Navbar";
+import Events from "./pages/Events";
+import CreateEvent from "./pages/CreateEvent";
+import EditEvent from "./pages/EditEvent";
+import Calendar from "./pages/Calendar";
+import "./App.css";
+
 
 function App() {
   
@@ -17,6 +22,10 @@ function App() {
       <Route path="/Login" element={<Login />} />
       <Route path="/Register" element={<Register />} />
       <Route path="/Homepage" element={<Homepage/>} />
+      <Route path="/events" element={<Events />} />
+      <Route path="/create-event" element={<CreateEvent />} />
+      <Route path="/edit-event/:id" element={<EditEvent />} />
+      <Route path="/calendar" element={<Calendar />} />
      </Routes>
 
     </div>
