5fdd2603283c3c3e4fbea510dda3cca12d10d6b2
Sam Figard <sffigard@gmail.com>
2025-05-04 15:50:27 -0400

    updates

--- login-screen/src/App.jsx
+++ login-screen/src/App.jsx
@@ -4,19 +4,9 @@ import Login from './components/Login'
 import { Route, Routes } from 'react-router-dom'
 import Register from './components/Register'
 import Homepage from './components/Homepage'
-<<<<<<< HEAD
 import MyEvents from './pages/MyEvents'
 import CreateEvent from './pages/CreateEvent'
 import EditEvent from './pages/EditEvent'
-=======
-import Navbar from "./components/Navbar";
-import Events from "./pages/Events";
-import CreateEvent from "./pages/CreateEvent";
-import EditEvent from "./pages/EditEvent";
-import Calendar from "./pages/Calendar";
-import "./App.css";
-
->>>>>>> 74227f3aac02e451e58c98aafd59c94d546b6ecd
 
 function App() {
   
@@ -28,17 +18,10 @@ function App() {
       <Route path="/Login" element={<Login />} />
       <Route path="/Register" element={<Register />} />
       <Route path="/Homepage" element={<Homepage/>} />
-<<<<<<< HEAD
       <Route path='/my-events' element={<MyEvents/>}/>
       <Route path='create-event' element={<CreateEvent/>}/>
       <Route path='edit-event/:id' element={<EditEvent/>}/> 
       
-=======
-      <Route path="/events" element={<Events />} />
-      <Route path="/create-event" element={<CreateEvent />} />
-      <Route path="/edit-event/:id" element={<EditEvent />} />
-      <Route path="/calendar" element={<Calendar />} />
->>>>>>> 74227f3aac02e451e58c98aafd59c94d546b6ecd
      </Routes>
     </div>
   )
